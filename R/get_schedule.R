5d0215d126c88a6389a13726d353cc2df78ecff2
Nehemias Ulloa <nulloa1@iastate.edu>
2023-10-11 20:02:35 -0700

    Add initial parts to github

---
+++ R/get_schedule.R
@@ -0,0 +1,19 @@
+#' Get the NFL schedule for a certain year
+#'
+#' @param year Year of NFL schedule
+#' @param orig Return original data or transformed data
+#' @return A data.frame
+#' @export
+#' @examples
+#' get_schedule(year="2023")
+
+get_schedule <- function(year=NULL, orig=F){
+  # Create the link from where to get the data
+  link <- paste0("https://www.4for4.com/teams/schedule/", year, "/grid")
+  # Pull the data
+  web_page <- rvest::read_html(link)
+  # Parse the data
+  odat <- rvest::html_table(rvest::html_element(web_page, "table"))
+  # Transform the data
+  tdat <- unique(do.call("rbind", apply(odat, 1, transform_data)))
+}
---
+++ R/transform_data.R
@@ -0,0 +1,15 @@
+#' Transform a row of data from \func{get_schedule}
+#'
+#' @param x The data from \func{get_schedule}
+#' @return A data.frame.
+#' @export
+
+transform_data <- function(x){
+  tdat <- data.frame(A = as.character(x[1]), B = as.character(t(x[-1])))
+  tdat$Week <- as.numeric(gsub("\\D", "", names(x)[-1]))
+  tdat$Home <- gsub("@", "", ifelse(grepl("@", tdat$B), tdat$B, tdat$A))
+  tdat$Away <- ifelse(grepl("@", tdat$B), tdat$A, tdat$B)
+  tdat <- tdat[,-c(1,2)]
+  tdat <- subset(tdat, Away!="BYE")
+  return(tdat)
+}
