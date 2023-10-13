af336cb8b84f262db89f3a61bd76af38ecda69aa
Nehemias Ulloa <nulloa1@iastate.edu>
2023-10-12 23:28:23 -0700

    Change the parameter name and add return ifelse

--- R/get_schedule.R
+++ R/get_schedule.R
@@ -1,13 +1,13 @@
 #' Get the NFL schedule for a certain year
 #'
 #' @param year Year of NFL schedule
-#' @param orig Return original data or transformed data
+#' @param original Return original data or transformed data
 #' @return A data.frame
 #' @export
 #' @examples
 #' get_schedule(year="2023")
 
-get_schedule <- function(year=NULL, orig=F){
+get_schedule <- function(year=NULL, original=F){
   # Create the link from where to get the data
   link <- paste0("https://www.4for4.com/teams/schedule/", year, "/grid")
   # Pull the data
@@ -16,4 +16,12 @@ get_schedule <- function(year=NULL, orig=F){
   odat <- rvest::html_table(rvest::html_element(web_page, "table"))
   # Transform the data
   tdat <- unique(do.call("rbind", apply(odat, 1, transform_data)))
+  
+  # Return original or transformed data
+  if(original){
+    return(odat)
+  }else{
+    return(tdat)
+  }
+  
 }
